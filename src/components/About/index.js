5e36478fe20c8d28dcaae4eac6baf8ac3aab097c
konadaprasad <konadaprasad1122@gmail.com>
2024-01-08 14:58:10 +0000

    routing practise1

---
+++ src/components/About/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const About = () => (
+  <div className="home-cont">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png "
+      alt="about"
+      className="home-img"
+    />
+    <h1 className="heading">About</h1>
+    <p className="para1">I love to creat! I am frontend developer</p>
+  </div>
+)
+export default About
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const Home = () => (
+  <div className="home-cont">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-blog-img.png"
+      alt="home"
+      className="home-img"
+    />
+    <h1 className="heading">Home</h1>
+  </div>
+)
+export default Home
